7e92d6f80f61845253f35e3fa3ba0e6fefa843d0
virgil2019 <danten0402@gmail.com>
2022-02-06 14:30:35 +0800

    Change test methods for NFT

--- kinghonorEthereum.js
+++ kinghonorEthereum.js
@@ -8,20 +8,45 @@ const web3 = new Web3('https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa445
 let testAccountPrivateKey = fs.readFileSync('.secret');
 testAccountPrivateKey = JSON.parse(testAccountPrivateKey).key;
 
-let NFTContractAddress = '0x3Aa7f1A87176d1fEE22E53698B7508cf9E18c322';
-let crossChainContractAddress = '0x014D1B7c2Be089C963b3055411DD76559B8c4eF9';
+let NFTContractAddress = '0x279Dbee63260750A621ee8498B3Afc89d4A8647f';
+let crossChainContractAddress = '0x9BF510E946B8470DbeBeBe0aC2BbD325e2e58270';
 
 let NFTRawData = fs.readFileSync('./KingHonorNFTView.json');
 let NFTAbi = JSON.parse(NFTRawData).abi;
 let NFTContract = new web3.eth.Contract(NFTAbi, NFTContractAddress);
 
+let CrossChainRawData = fs.readFileSync('./MultiPorterCrossChain.json');
+let CrossChainAbi = JSON.parse(CrossChainRawData).abi;
+let CrossChainContract = new web3.eth.Contract(CrossChainAbi, crossChainContractAddress);
+
 async function init() {
-    // set cross chain contract
-    await avalanche.sendTransaction(NFTContract, 'setCrossChainContract', testAccountPrivateKey, [crossChainContractAddress]);
+    // // register porters
+    // await avalanche.sendTransaction(CrossChainContract, 'changePortersAndRequirement', testAccountPrivateKey, [['0x30ad2981E83615001fe698b6fBa1bbCb52C19Dfa'], 1]); 
+    // // set cross chain contract
+    // await avalanche.sendTransaction(NFTContract, 'setCrossChainContract', testAccountPrivateKey, [crossChainContractAddress]);
     // // register interface
-    await avalanche.sendTransaction(NFTContract, 'registerInterface', testAccountPrivateKey, ['mintTo', '{"inputs":[{"internalType":"address","name":"_to","type":"address"}],"name":"mintTo","outputs":[],"stateMutability":"nonpayable","type":"function"}']);
-    await avalanche.sendTransaction(NFTContract, 'registerInterface', testAccountPrivateKey, ['crossChainTransferFrom', '{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"crossChainTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}']);
-    await avalanche.sendTransaction(NFTContract, 'registerInterface', testAccountPrivateKey, ['crossChainSafeTransferFrom', '{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"crossChainSafeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}']);
+    // await avalanche.sendTransaction(NFTContract, 'registerInterface', testAccountPrivateKey, ['mintTo', '{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mintTo","outputs":[],"stateMutability":"nonpayable","type":"function"}']);
+    // await avalanche.sendTransaction(NFTContract, 'registerInterface', testAccountPrivateKey, ['crossChainTransferFrom', '{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"crossChainTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}']);
+    // await avalanche.sendTransaction(NFTContract, 'registerInterface', testAccountPrivateKey, ['crossChainSafeTransferFrom', '{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"crossChainSafeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}']);
+    // transfer ownership
+    await avalanche.sendTransaction(NFTContract, 'transferOwnership', testAccountPrivateKey, [crossChainContractAddress]);
 }
 
-init();
+async function test() {
+    let info = [
+        '1',
+        'AVALANCHE',
+        '0xe37a7a25183609Bb553757cB8274797522c52100',
+        '0x3Aa7f1A87176d1fEE22E53698B7508cf9E18c322',
+        'mintTo',
+        '0x755edd17000000000000000000000000e8df0d0f31007311ae25a2a207565d1c350ac1b7'
+      ]
+    // register porters
+    // await avalanche.sendTransaction(CrossChainContract, 'receiveMessage', testAccountPrivateKey, info); 
+
+    let aa = await avalanche.contractCall(NFTContract, 'ownerOf', [1]);
+    console.log('aa', aa);
+}
+
+// init();
+test();
--- kinghonorPlatON.js
+++ kinghonorPlatON.js
@@ -8,29 +8,45 @@ const web3 = new Web3('http://35.247.155.162:6789');
 let testAccountPrivateKey = fs.readFileSync('.secret');
 testAccountPrivateKey = JSON.parse(testAccountPrivateKey).key;
 
-let NFTContractAddress = '0xe37a7a25183609Bb553757cB8274797522c52100';
-let crossChainContractAddress = '0xAF8028Ea3c2D889482F1B9092f1792B8414ceC1A';
+let NFTContractAddress = '0x6Ba41d02A32F9DbB04868BEb7D12F77C3cF575Aa';
+let crossChainContractAddress = '0xb6Ad052fd1c4bB8bD2dF1d91539103b481bF724a';
 
 let NFTRawData = fs.readFileSync('./KingHonorNFT.json');
 let NFTAbi = JSON.parse(NFTRawData).abi;
 let NFTContract = new web3.eth.Contract(NFTAbi, NFTContractAddress);
 
+let CrossChainRawData = fs.readFileSync('./MultiPorterCrossChain.json');
+let CrossChainAbi = JSON.parse(CrossChainRawData).abi;
+let CrossChainContract = new web3.eth.Contract(CrossChainAbi, crossChainContractAddress);
+
 async function init() {
-    // set cross chain contract
+    // // set cross chain contract
     await avalanche.sendTransaction(NFTContract, 'setCrossChainContract', testAccountPrivateKey, [crossChainContractAddress]);
-    // register target
+    // // register target
     await avalanche.sendTransaction(NFTContract, 'registerTarget', testAccountPrivateKey, ['mintTo', 'address', 'to', '']);
     await avalanche.sendTransaction(NFTContract, 'registerTarget', testAccountPrivateKey, ['transferFrom', 'address,address,uint256', 'from,to,tokenId', '']);
     await avalanche.sendTransaction(NFTContract, 'registerTarget', testAccountPrivateKey, ['safeTransferFrom', 'address,address,uint256,bytes', 'from,to,tokenId,data', '']);
     // register method
-    await avalanche.sendTransaction(NFTContract, 'registerDestinationMethod', testAccountPrivateKey, ['ETHEREUM', '0x014D1B7c2Be089C963b3055411DD76559B8c4eF9', 'mintTo']);
-    await avalanche.sendTransaction(NFTContract, 'registerDestinationMethod', testAccountPrivateKey, ['ETHEREUM', '0x014D1B7c2Be089C963b3055411DD76559B8c4eF9', 'crossChainTransferFrom']);
-    await avalanche.sendTransaction(NFTContract, 'registerDestinationMethod', testAccountPrivateKey, ['ETHEREUM', '0x014D1B7c2Be089C963b3055411DD76559B8c4eF9', 'crossChainSafeTransferFrom']);
+    await avalanche.sendTransaction(NFTContract, 'registerDestinationMethod', testAccountPrivateKey, ['ETHEREUM', '0x279Dbee63260750A621ee8498B3Afc89d4A8647f', 'mintTo']);
+    await avalanche.sendTransaction(NFTContract, 'registerDestinationMethod', testAccountPrivateKey, ['ETHEREUM', '0x279Dbee63260750A621ee8498B3Afc89d4A8647f', 'crossChainTransferFrom']);
+    await avalanche.sendTransaction(NFTContract, 'registerDestinationMethod', testAccountPrivateKey, ['ETHEREUM', '0x279Dbee63260750A621ee8498B3Afc89d4A8647f', 'crossChainSafeTransferFrom']);
 }
 
-// async function sendGreeting() {
-//     await avalanche.sendTransaction(NFTContract, 'sendGreeting', testAccountPrivateKey, ['NEAR', 'Greetings', 'Greeting from Avalanche', '2022-01-01']);
-// }
+async function mintTo() {
+    await avalanche.sendTransaction(NFTContract, 'mintTo', testAccountPrivateKey, ['0xE8dF0d0f31007311aE25a2a207565D1C350AC1B7']);
+}
 
-init();
-// sendGreeting();
+async function transferTo(priKey, to, id) {
+    const from = web3.eth.accounts.privateKeyToAccount(priKey).address;
+    console.log(from, to, id);
+    await avalanche.sendTransaction(NFTContract, 'transferFrom', priKey, [from, to, id]);
+}
+
+async function clear() {
+    await avalanche.sendTransaction(CrossChainContract, 'clearCrossChainMessage', testAccountPrivateKey, ['ETHEREUM']);
+}
+
+// init();
+// mintTo();
+// transferTo('e57e84cb1a46e720caf15c225426169f8dde9fb56b5260eceb12a18b07231f6a', '0x5787b31b60FF997268325fC55723be072c6D23c7', 1);
+// clear();
